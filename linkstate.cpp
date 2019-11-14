b927f36ffcc69a6b1f58ecdf66e8c5a99db61ba4
everetttsang <everettt@usc.edu>
2019-11-14 03:48:31 -0800

    Implementing Dijkstra
    Implement Dijkstra in progress. Incomplete

--- linkstate.cpp
+++ linkstate.cpp
@@ -19,6 +19,28 @@ struct Node
   map<int, int> neighbors;
 
 };
+
+void printList(vector<int> v){
+  cout << "[";
+  for (int i=0; i< v.size(); i++){
+    cout<< "\'"<<v.at(i)<<"\'";
+    if (i != v.size()-1)
+      cout<<",";
+    }
+    cout <<"]"<<endl;
+  }
+
+
+
+int existsIn(vector<int> nodes, int nodeId){
+  for (int i=0; i< nodes.size(); i++){
+    if ( nodes.at(i) == nodeId){
+      return i;
+    }
+  }
+  return -1;
+
+}
 int generateGraph(){
   string line;
   ifstream graphData ("Adj.txt");
@@ -38,7 +60,7 @@ int generateGraph(){
 
       ss<< line;
       ss>> nodeA >> nodeB >> lengthAB;
-      cout << nodeA<< " " << nodeB <<" "<< lengthAB<<endl;
+      // cout << nodeA<< " " << nodeB <<" "<< lengthAB<<endl;
 
       //cout<< "Graph size: "<< graph.size()<<endl;
       for (int i=0; i< graph.size(); i++){
@@ -53,21 +75,21 @@ int generateGraph(){
         temp.id= stoi(nodeA);
         temp.neighbors.insert(pair<int,int>(stoi(nodeB), stoi(lengthAB)));
         graph.push_back(temp);
-        cout << "Node: "<< temp.id << "created!"<<endl;
+        // cout << "Node: "<< temp.id << "created!"<<endl;
 
       }
       else{
-        cout<<"Node " <<foundIndex << " was found!"<<endl;
+        //cout<<"Node " <<foundIndex << " was found!"<<endl;
         graph.at(foundIndex).neighbors.insert(pair<int,int>(stoi(nodeB), stoi(lengthAB)));
       }
 
 
     }
     for (int i=0; i< graph.size(); i++){
-      cout << "Node " << graph.at(i).id << "------"<<endl;
+      // cout << "Node " << graph.at(i).id << "------"<<endl;
       map<int, int>:: iterator itr;
       for (itr = graph.at(i).neighbors.begin(); itr!=graph.at(i).neighbors.end(); ++itr){
-        cout << "Neighbor: " << itr-> first<< " Distance: "<< itr->second<<endl;
+        //cout << "Neighbor: " << itr-> first<< " Distance: "<< itr->second<<endl;
       }
     }
   }
@@ -76,7 +98,56 @@ int generateGraph(){
   }
   return 0;
 }
+
+int dijkstra(int dest, int source){
+  vector<int> visited;
+  vector<int> unvisited;
+  int shortestPath=0;
+  int currentNode;
+
+  //populate unvisited list
+  for (int i=0; i< graph.size(); i++){
+    unvisited.push_back(i);
+  }
+
+  int leastCostNode;
+  int leastCost=INT_MAX;
+  currentNode= source;
+  visited.push_back(currentNode);
+  map<int, int>:: iterator itr;
+  int key;
+  int value;
+
+  cout << "Visited: ";
+  printList(visited);
+  cout <<endl<<"Unvisited: ";
+  printList(unvisited);
+  cout<<endl;
+
+  for (itr = graph.at(dest).neighbors.begin(); itr!=graph.at(dest).neighbors.end(); ++itr){
+    key = itr->first;
+    value = itr->second;
+    cout << "Neighbor: " << key<< " Distance: "<< value<<endl;
+    if (key < leastCost && existsIn(visited, key==-1)){
+      leastCostNode = key;
+      leastCost = value;
+    }
+  }
+  cout <<"Least cost node: "<<leastCostNode<<endl;
+  shortestPath+= leastCost;
+  vector<int>::iterator ptr = unvisited.begin();
+  advance(ptr, existsIn(unvisited,currentNode));
+  unvisited.erase(ptr);
+  currentNode = leastCostNode;
+
+  cout <<"Went to this node: "<<currentNode<<endl;
+
+
+
+return 0;
+}
 int main(){
   generateGraph();
+  dijkstra(0,1);
   return 0;
 }
